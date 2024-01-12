0ea2d355da6fcf9791085889546ae53deca8c0e1
Chris <138630942+CrisCo116@users.noreply.github.com>
2024-01-12 15:56:39 -0800

    reworked

--- index.js
+++ index.js
@@ -1,127 +1,85 @@
-// Imported libraries
-const fs = require('fs-extra'); // File system operation
-const inquirer = require('inquirer'); // User prompt library
-const buildSVG = require('./lib/svg.js'); // Custom library to build SVG
-const Color = require('color'); // Color validation library
+const inquirer = require("inquirer");
+const fs = require ("fs");
+const {Circle, Rectangle, Triangle} = require("./lib/shapes");
 
-// Function that takes user input and generates a logo
-async function generateLogo() {
-	try {
-		const logoParams = await inquirer.prompt([
-			// Input prompts for logo parameters
-			{
-				type: 'input',
-				name: 'logo',
-				message:
-					'Enter the characters used in your logo (limit of 3 characters)',
-				validate: (input) => input.length <= 3,
-			},
+// generates SVG from the user data
+function generateSVG(shapeChoice, { letters, textColor, shapeColor }) {
+    
+    const chosenShape = shapeChoice.render();
+    const textElement = `<text x="150" y="100" dominant-baseline="middle"  stroke-width="0" stroke="#000" font-size="24px" text-anchor="middle" fill="${textColor}">${letters}</text>`;
+  
+    const svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200" fill = ${shapeColor}>${chosenShape}${textElement}</svg>`;
+    return svg;
+  }
 
-			{
-				type: 'list',
-				name: 'shape',
-				message: 'Choose the shape you would like to use',
-				choices: ['Triangle', 'Circle', 'Square'],
-			},
+//   questions for user to help create SVG
+const questions = [
+    {
+      type: "input",
+      message: "Please enter 3 characters",
+      name: "letters",
+    }, 
+    {
+      type: "input",
+      message: "what color whould you like the text to be?",
+      name: "textColor",
+  
+    },
+    {
+      type: "list",
+      message: "What shape would you like your logo to be?",
+      choices: ['Triangle', 'Rectangle', 'Circle'],
+      name:"logo",
+    },
+    {
+      type: "input",
+      message: "What color would you like your shape to be?",
+      name:"shapeColor"
+    }
+  
+  ];
 
-			{
-				type: 'list',
-				name: 'textColor',
-				message: 'Choose your text color',
-				choices: ['Black', 'White', 'Royal Blue', 'Tomato', 'Custom'],
+function svgFile (fileName , data) {
+    fs.writeFile(fileName,data, (err) => {
+        if(err){
+            console.log(err);
+        } else {
+            console.log(`${fileName} has been created successfully`);
+        }
 
-				// Converts choice to lower case and removes spaces for processing
-				filter: (choice) =>
-					choice.toLowerCase().replace(/ /g, '') === 'custom'
-						? 'custom'
-						: choice.toLowerCase().replace(/ /g, ''),
-			},
+    })
 
-			// Allows a user to enter a custom text color
-			{
-				type: 'input',
-				name: 'customTextColor',
-				message:
-					'Enter your custom text color (can be hexadecimal(#000808) or color name)',
-				filter: (choice) => choice.toLowerCase().replace(/ /g, ''),
-				when: (answers) => answers.textColor === 'custom',
-			},
-
-			{
-				type: 'list',
-				name: 'colorChoice',
-				message: 'Pick the color you want the shape to be',
-				choices: ['Black', 'Dark Red', 'Olive', 'Hot Pink', 'Custom'],
-
-				// Converts choice to lower case and removes spaces for processing
-				filter: (choice) =>
-					choice.toLowerCase().replace(/ /g, '') === 'custom'
-						? 'custom'
-						: choice.toLowerCase().replace(/ /g, ''),
-			},
-
-			// Allows user to enter a custom shape fill color
-			{
-				type: 'input',
-				name: 'customShapeColor',
-				message:
-					'Enter your custom shape color (can be hexadecimal (#181818) or color name)',
-				filter: (choice) => choice.toLowerCase().replace(/ /g, ''),
-				when: (answers) => answers.colorChoice === 'custom',
-			},
-		]);
-
-		// Destructures user input into an object
-		const {
-			logo,
-			shape,
-			textColor,
-			customTextColor,
-			colorChoice,
-			customShapeColor,
-		} = logoParams;
-
-		// Determines text and shape colors depending on user input
-		const textColorChoice =
-			textColor === 'custom' ? customTextColor : textColor;
-		const shapeColorChoice =
-			colorChoice === 'custom' ? customShapeColor : colorChoice;
-
-		// Validate chosen custom colors
-		if (!validColor(textColorChoice) || !validColor(shapeColorChoice)) {
-			console.error(
-				"One of the custom colors was invalid. Please use hexadecimal (include '#') values or color names."
-			);
-			return;
-		}
-
-		// Create SVG logo using user input values
-		const svgCreator = buildSVG({
-			logo,
-			shape,
-			textColor: textColorChoice,
-			colorChoice: shapeColorChoice,
-		});
-
-		// Write the generated SVG logo to a file in 'examples' directory
-		await fs.writeFile('./examples/logo.svg', svgCreator);
-		console.log('Generated logo.svg');
-	} catch (error) {
-		console.error('An error occurred:', error);
-	}
 }
 
-// Function to validate color choices
-function validColor(color) {
-	try {
-		// Use 'color' library to validate color format
-		Color(color);
-		return true;
-	} catch (error) {
-		// Return false if format is invalid
-		return false;
-	}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      // checks to see if user went over 3 letters
+      if (answers.letters.length < 0 || answers.letters.length > 3) {
+        console.log("Please do not exceed 3 letters");
+      } else {
+        let shapeChoice;
+        // switch case to determine what shape the user wants
+        switch (answers.logo) {
+          case 'Triangle':
+            shapeChoice = new Triangle();
+            break;
+          case 'Rectangle':
+            shapeChoice = new Rectangle();
+            break;
+          case 'Circle':
+            shapeChoice = new Circle();
+            break;
+          default:
+            console.error('Invalid shape');
+            return;
+        }
+        const svgData = generateSVG(shapeChoice, answers);
+        svgFile("./examples/logo.svg", svgData);
+      }
+    });
 }
 
-// Runs logo generating function when script is executed
-generateLogo();
+  // calling the function
+  init();
